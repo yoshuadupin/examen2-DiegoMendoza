27dff45da503e47a09e306fb4400da4cdca35048
yoshuadupin <djosue@united.edu>
2018-09-16 15:12:50 -0600

    Comentarios y Likes

--- components/Post.js
+++ components/Post.js
@@ -1,63 +1,147 @@
 import React, { Component } from "react";
-import { View, StyleSheet , Image } from "react-native";
-import * as firebase from "firebase"
-import { Card, CardItem, Thumbnail, Text, Button, Icon, Left, Body } from 'native-base';
+import { View, StyleSheet, Image } from "react-native";
+import * as firebase from "firebase";
+import {
+  Card,
+  CardItem,
+  Thumbnail,
+  Text,
+  Button,
+  Icon,
+  Left,
+  Body
+} from "native-base";
+
 
 class Post extends Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            /*
-            expanded: false,
-            comments: {},
-            writeComment: false,
-            newComment: "",
-            title: this.props.title,
-            visibility: PUBLIC,
-            content: this.props.content,
-            username: this.props.username,
-            contFav: this.props.contFav,
-            countComment: this.props.countComment,
-            addFav: this.props.favs,
-            follow: this.props.follow*/
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      //      expanded: false,
+      //    comments: {},
+      //   writeComment: false,
+      //  newComment: "",
+      // title: this.props.title,
+      //visibility: PUBLIC,
+      //content: this.props.content,
+      //username: this.props.username,
+      contFav: this.props.contFav,
+      //countComment: this.props.countComment,
+      addFav: false
+      //follow: this.props.follow
+    };
+    this.handleFavClick = this.handleFavClick.bind(this);
+      console.log("CONTFAV")
+    console.log(this.state.contFav)
+  }
 
+  handleFavClick = () => {
+    this.updateFavPost();
+    this.setState(state => ({
+      addFav: !state.addFav,
+      contFav: contFav
+    }));
+  };
+  updateFavPost() {
+    contFav = this.state.contFav;
+    const uid = firebase.auth().currentUser.uid;
+
+    if (!this.state.addFav) {
+      
+      firebase
+        .database()
+        .ref(`posts/${this.props.id}/favs/${uid}`)
+        .set(true, function(error) {
+          if (error) {
+            console.log("No guardo");
+          } else {
+            console.log("Si guardo" + contFav);
+          }
+        })
+        .then(
+          firebase
+            .database()
+            .ref("users/" + uid + "/favs")
+            .push()
+            .set({ idPost: this.props.id })
+        );
+
+      contFav++;
+    } else {
+      
+      firebase
+        .database()
+        .ref("posts")
+        .child(this.props.id)
+        .child("favs")
+        .child(uid)
+        .set(null, function(error) {
+          if (error) {
+            console.log("No guardo");
+          } else {
+            console.log("Si guardo" + contFav);
+          }
+        });
+      let tempFavKey;
+      firebase
+        .database()
+        .ref("users")
+        .child(uid)
+        .child("favs")
+        .on("value", snapshot => {
+          snapshot.forEach(data => {
+            if (data.val().idPost == this.props.id) {
+              tempFavKey = data.key;
+              console.log(tempFavKey);
+            }
+          });
+        });
+
+      firebase
+        .database()
+        .ref("users/" + uid + "/favs/" + tempFavKey)
+        .set(null);
+        
+      contFav--;
+    }
+  }
+
+ 
   render() {
     return (
-     
-        <Card style={{flex: 0}}>
-          <CardItem>
-            <Left>
-              <Thumbnail source={{uri:'https://cdn1.iconfinder.com/data/icons/mix-color-4/502/Untitled-1-512.png' }} />
-              <Body>
-                <Text>{this.props.title}</Text>
-                <Text note>{this.props.displayname}</Text>
-              </Body>
-            </Left>
-          </CardItem>
-          <CardItem>
+      <Card style={{ flex: 0 }}>
+        <CardItem>
+          <Left>
+            <Thumbnail
+              source={{
+                uri:
+                  "https://cdn1.iconfinder.com/data/icons/mix-color-4/502/Untitled-1-512.png"
+              }}
+            />
             <Body>
-              <Text>
-                {this.props.content}
-              </Text>
+              <Text>{this.props.title}</Text>
+              <Text note>{this.props.displayname}</Text>
             </Body>
-          </CardItem>
-          <CardItem>
-            <Left>
-              <Button transparent textStyle={{color: '#87838B'}}>
-                <Icon name="ios-heart" />
-                <Text>{this.props.contFav}</Text>
-              </Button>
-              <Button transparent textStyle={{color: '#87838B'}}>
-                <Icon name="ios-chatboxes" />
-                <Text>{this.props.countComment}</Text>
-              </Button>
-            </Left>
-          </CardItem>
-        </Card>
-      
-      
+          </Left>
+        </CardItem>
+        <CardItem>
+          <Body>
+            <Text>{this.props.content}</Text>
+          </Body>
+        </CardItem>
+        <CardItem>
+          <Left>
+            <Button onPress ={()=>this.handleFavClick()} transparent  textStyle={{ color: "#87838B" }}>
+              <Icon name="ios-heart"  />
+              <Text>{this.state.contFav}</Text>
+            </Button>
+            <Button transparent textStyle={{ color: "#87838B" }}>
+              <Icon name="ios-chatboxes" />
+              <Text>{this.props.countComment}</Text>
+            </Button>
+          </Left>
+        </CardItem>
+      </Card>
     );
   }
 }
@@ -68,7 +152,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    
+    alignItems: "center"
   }
 });
--- screens/DashBoardScreen.js
+++ screens/DashBoardScreen.js
@@ -1,29 +1,37 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet, Button, FlatList } from "react-native";
+import { View, StyleSheet, Button, FlatList } from "react-native";
 import * as firebase from "firebase";
 import Post from "../components/Post";
-import { Container, Header, Content } from 'native-base';
+import {
+  Container,
+  List,
+  ListItem,
+  Body,
+  Header,
+  Text,
+  DeckSwiper,
+} from "native-base";
+
 class DashBoardScreen extends Component {
   constructor(props) {
     super(props);
   }
-  
+
   state = {
     posts: [],
-    //user: Object.assign(firebaseAuth().currentUser),
+    user: Object.assign(firebase.auth().currentUser),
     visibility: "PUBLIC",
     public: true,
     private: false,
     follower: false,
     userFav: [],
-    userFollower:[]
+    userFollower: []
   };
-  
+
   componentWillMount() {
-   // this.loadFavPost();
-   // this.loadFavPost();
+    //this.loadFavPost();
+    // this.loadFavPost();
     this.loadDashboard();
-    console.log(this.state.posts)
   }
 
   loadDashboard() {
@@ -35,61 +43,100 @@ class DashBoardScreen extends Component {
       .on("child_added", snapshot => {
         if (snapshot.val().visibility == "PUBLIC") {
           tpost.push(snapshot.val());
-        console.log(snapshot.val())
         }
-        this.setState( ({
+        console.log(snapshot.val());
+        this.setState({
           //posts: this.state.posts.concat(snapshot.val())
           posts: tpost
-        }));
+        });
       });
-    
-    
   }
-/*
+
   loadFavPost() {
     let tfavs = [];
-    firebase
-      .database()
-      .ref("users")
-      .child(this.state.user.uid)
-      .child("favs")
-      .on("value", snapshot => {
-        //TODO FOR EACH FAVORITOS Y EVALUAR Y MARCARLOS
-        snapshot.forEach(data => {
-          tfavs.push(data.val().idPost);
-        });
+    if (this.state.user != null) {
+      console.log(this.state.user);
+      firebase
+        .database()
+        .ref("users")
+        .child(this.state.user.uid)
+        .child("favs")
+        .on("value", snapshot => {
+          //TODO FOR EACH FAVORITOS Y EVALUAR Y MARCARLOS
+          snapshot.forEach(data => {
+            tfavs.push(data.val().idPost);
+          });
 
-        this.setState(state => ({
-          //posts: this.state.posts.concat(snapshot.val())
-          userFav: tfavs
-        }));
-      });
+          this.setState(state => ({
+            //posts: this.state.posts.concat(snapshot.val())
+            userFav: tfavs
+          }));
+        });
+    }
   }
-  */
+
+  /*
+ loadFollower() {
+  let tfollow = [];
+
+  firebase
+    .database()
+    .ref("users")
+    .child(this.state.user.uid)
+    .child("followers")
+    .on("value", snapshot => {
+      //TODO FOR EACH FAVORITOS Y EVALUAR Y MARCARLOS
+      snapshot.forEach(data => {
+        tfollow.push(data.val().uidFollow);
+      });
+
+      this.setState(state => ({
+        //posts: this.state.posts.concat(snapshot.val())
+        userFollower: tfollow
+      }));
+    });
+}
+*/
   render() {
     return (
       <Container>
-      <Header title ="Goatter Mobile"></Header>
-         <FlatList
-          data={this.state.posts.reverse()}
-          keyExtractor={(item) => item.id}
-          renderItem={({ item }) =>  <Post
-                  //favs={this.state.userFav.includes(item.id)}
-                  //comments={item.comments}
-                  countComment = {item.countComment}
-                  title={item.title}
-                  content={item.content}
-                  contFav={item.contFav}
-                  displayname={item.displayname}
-                  photoUrl={item.photoUrl}
-                  id={item.id}
-                  uid={item.uid}
-                  //public={this.isPublic(item.visibility)}
-                  //private={this.isPrivate(item.visibility)}
-                  //follower={this.isFollower(item.visibility)}
-                  //follow={this.state.userFollower.includes(item.uid)}
-
-                />}
+        <Header title="Goatter Mobile" />
+        <DeckSwiper
+          dataSource={this.state.posts}
+          //renderEmpty = {()=><View>
+          //  <Text>No hay mas publicaciones</Text>
+          //</View>}
+          //keyExtractor={item => item.id}
+          renderItem={item => (
+            <View>
+              <Post
+                //favs={this.state.userFav.includes(item.id)}
+                //comments={item.comments}
+                countComment={item.countComment}
+                title={item.title}
+                content={item.content}
+                contFav={item.contFav}
+                displayname={item.displayname}
+                photoUrl={item.photoUrl}
+                id={item.id}
+                uid={item.uid}
+                //public={this.isPublic(item.visibility)}
+                //private={this.isPrivate(item.visibility)}
+                //follower={this.isFollower(item.visibility)}
+                //follow={this.state.userFollower.includes(item.uid)}
+              />
+              {item.comments ?<List
+                dataArray={item.comments}
+                renderRow={value => (
+                  <ListItem>
+                    <Body>
+                      <Text>{value.comment}</Text>
+                    </Body>
+                  </ListItem>
+                )}
+              />:<View></View>}
+            </View>
+          )}
         />
       </Container>
     );
